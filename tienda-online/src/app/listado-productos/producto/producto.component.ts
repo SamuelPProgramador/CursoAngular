25847ea25d28d6d86fab745f0c5543515d1e4d5d
Samuel <samuelariaspenalo.com>
2025-02-06 23:20:44 -0400

    hacienco el ejercicio correpondiente a la unidad

---
+++ tienda-online/src/app/listado-productos/producto/producto.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  standalone: true,
+  imports: [],
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.css'
+})
+export class ProductoComponent {
+  descripcion = "Nuevo Producto";
+  precio = 100;
+
+}
