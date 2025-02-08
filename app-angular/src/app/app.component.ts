6f7def1829bca75894cfc4343fda954675284611
Samuel <samuelariaspenalo.com>
2025-02-08 07:41:35 -0400

    Estoy devuelta

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -4,11 +4,14 @@ import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.co
 import { ComponenteInlineComponent } from "./componente-inline/componente-inline.component";
 import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
 import { PadreComponent } from "./padre/padre.component";
+import { HijoComponent } from "./hijo/hijo.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, InterpolacionComponent, PadreComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, InterpolacionComponent, PadreComponent, HijoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
