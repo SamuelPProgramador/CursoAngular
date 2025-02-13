cb5796f34c8add199e2dd954b9d8437393418bf5
Samuel <samuelariaspenalo.com>
2025-02-13 07:40:50 -0400

    Sigo trabajando

--- app-calculadora/src/app/app.component.ts
+++ app-calculadora/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from "./formulario/formulario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ FormsModule],
+  imports: [FormsModule, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-calculadora';
-  operandoA: number = 0;
-  operandoB: number = 0;
-  resultado: number = 0;
-  sumar(){
-     return this.resultado = this.operandoA + this.operandoB;
-  }
 
+  title = 'app-calculadora';
+  resultado: number = 0;
+  procesarResultado(resultado: number) {
+    this.resultado = resultado;
+  }
+  
   //agregar Restar, Multiplicar y Dividir...
   
 }
--- app-calculadora/src/app/formulario/formulario.component.ts
+++ app-calculadora/src/app/formulario/formulario.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  operandoA: number | null =  null;
+  operandoB: number | null = null;
+  
+  @Output() resultadoSuma = new EventEmitter<number>();
+
+  sumar(){
+    if(this.operandoA && this.operandoB){
+      const resultado = this.operandoA + this.operandoB;
+      this.resultadoSuma.emit(resultado);
+    }else{
+      console.log("Se debe introducir los valores correcto...");
+    }
+ }
 
 }
