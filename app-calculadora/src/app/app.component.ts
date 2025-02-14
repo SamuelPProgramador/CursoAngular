48c09b7f07ea2ea8c37c61b62f3e2436482f8660
Samuel <samuelariaspenalo.com>
2025-02-14 07:21:33 -0400

    sigo trabajando

--- app-calculadora/src/app/app.component.ts
+++ app-calculadora/src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FormularioComponent } from "./formulario/formulario.component";
+import { ResultadoComponent } from "./resultado/resultado.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, FormularioComponent],
+  imports: [FormsModule, FormularioComponent, ResultadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   title = 'app-calculadora';
-  resultado: number = 0;
-  procesarResultado(resultado: number) {
-    this.resultado = resultado;
+  resultadoPadre: number = 0;
+  procesarResultado(resultadoHijo: number) {
+    this.resultadoPadre = resultadoHijo;
   }
   
   //agregar Restar, Multiplicar y Dividir...
--- app-calculadora/src/app/resultado/resultado.component.ts
+++ app-calculadora/src/app/resultado/resultado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resultado',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './resultado.component.css'
 })
 export class ResultadoComponent {
+  @Input() resultadoHijo: number | null = null;
+  
+
 
 }
