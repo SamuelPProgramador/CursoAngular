b8c80df32fded530373ef9ae672fda75f30ceec2
Samuel <samuelariaspenalo.com>
2025-02-14 07:35:44 -0400

    nuevas funciones

--- app-angular/src/app/view-child/view-child.component.ts
+++ app-angular/src/app/view-child/view-child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-view-child',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './view-child.component.css'
 })
 export class ViewChildComponent {
+  @ViewChild('referenciaInput') inputElemento! : ElementRef;
+
+  cambiarTexto(){
+    this.inputElemento.nativeElement.value = "Texto Cambiado ";
+  }
 
 }
