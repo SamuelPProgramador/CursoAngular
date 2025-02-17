32793f1bc241d426502d54092a6edd3b7ab93eed
Samuel <samuelariaspenalo.com>
2025-02-17 08:47:02 -0400

    acabado por ahora

--- tienda-online/src/app/listado-productos/producto/producto.component.ts
+++ tienda-online/src/app/listado-productos/producto/producto.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Producto } from './producto.model';
 
 @Component({
   selector: 'app-producto',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './producto.component.css'
 })
 export class ProductoComponent {
-  descripcion = "Nuevo Producto";
-  precio = 100;
+  @Input() producto!: Producto;
+  
 
 }
